339d48351b1696c82f75d2a4bfe019d1d33a52a8
itsYashKhare <kharey02@gmail.com>
2024-01-07 02:35:03 +0530

    creating react projects

---
+++ brandpage-1/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Navigation from './components/Navigation'
+import HeroSection from './components/Hero'
+
+function App() {
+  //javascript here
+  // const [count, setCount] = useState(0)
+
+  return (
+    // jsx code here
+
+    <div>
+      <Navigation/>
+      <HeroSection/>
+    </div>
+
+  )
+}
+
+export default App
